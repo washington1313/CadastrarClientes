6579cb154464e9791c91a66e71ed6c6bed442da5
washington1313 <123412352+washington1313@users.noreply.github.com>
2023-04-04 23:41:21 -0300

    Add files via upload

---
+++ Cadastrar Cliente/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastrar_Cliente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomeFuncionarios = new string[5];
+            int contador = 0;
+
+            while (contador < nomeFuncionarios.Length)
+            {
+                Console.WriteLine("Digite o nome do Funcionário:");
+                nomeFuncionarios[contador++] = Console.ReadLine();
+                Console.Clear();
+            }
+
+            int contador2 = 0;
+            while (contador2 < nomeFuncionarios.Length)
+            {
+                Console.WriteLine(nomeFuncionarios[contador2]);
+                contador2++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
